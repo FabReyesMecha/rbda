65773d3fd8faad6e422bef5473b5ff063657c935
Reyes Fabian <burgundianvolker@gmail.com>
2017-10-22 20:21:49 +0900

    new structure of files

---
+++ rbda_src (Python)/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='sym_rbda',
+      version='0.7.0',
+      description='Numeric and symbolic functions for rigid body simulation and analysis',
+      url='http://github.com/...',
+      author='Fabian Reyes',
+      author_email='burgundianvolker@gmail.com',
+      license='MIT',
+      packages=['sym_rbda'],
+      install_requires=[
+        'numpy',
+        'math',
+        'copy',
+        'sympy',
+        
+      ],
+      zip_safe=False)
